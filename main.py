9e6aa2be7634c82a94cf3f5cf18be9451928a899
haticereno <haticeceren.oruc@std.yeditepe.edu.tr>
2024-12-26 17:07:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-alter=int(input("Alter: "))
-note = int(input("Note:"))
+note =
+float(input("Abschlussnote(0-100): "))
+erfahrung =
+int(input("Programmierefahrung (1-5): "))
 
-if 20 <= alter <= 50 and note >80: 
+if note > 90 or (erfahrung == 5 and note >= 70):
   print("einstellen")
+elif note > 70 or erfahrung == 4:
+   print("zum Gesprach einladen")
 else:
   print("ablehnen")

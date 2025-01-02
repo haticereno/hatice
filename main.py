c3b2f3aed92f800670584e78a6c8a5a1e45a4531
haticereno <haticeceren.oruc@std.yeditepe.edu.tr>
2025-01-02 16:49:52 +0300

    hatice ceren oruç

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-# Startguthaben
-konto = 1000
-
-print("Ihr aktuelles Guthaben beträgt: {konto} EURO")
-aktion = input("Möchten Sie 'einzahlen' oder 'abheben'? ").strip().lower()
-
-if aktion == "einzahlen":
-    betrag = int(input("Wie viel möchten Sie einzahlen? "))
-    konto += betrag
-
-elif aktion == "abheben":
-    betrag = int(input("Wie viel möchten Sie abheben? "))
-    konto -= betrag
-
-print("Ihr aktuelles Guthaben beträgt jetzt: {konto} EURO")
+x1=int(input("x1"))
+y1=int(input("y1"))
+x2=int(input("x1"))
+y2=int(input("y1"))
+d=(y2-y1)**2+(x2-x1)**2
+print(d)
